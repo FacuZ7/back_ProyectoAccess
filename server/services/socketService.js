294a9ad365971106d3702c7dff08620d07c2358f
Facundo Zapata <facundozapata41@gmail.com>
2024-06-09 00:22:33 -0300

    bug de comillas en el promp inicial + app express json

--- server/services/socketService.js
+++ server/services/socketService.js
@@ -8,13 +8,13 @@ const socketService = (io) => {
             console.log('Se desconectó un usuario');
         });
         
-        socket.on('chat message', (msg) => {
+        socket.on('chat message', async (msg) => {
             console.log("Tercer Paso: El servidor recibe lo que el usuario haya escrito")
             console.log("Aca deberíamos enviarle el mensaje a OPENAI y que haga su magia")
-            // const {answer} = await generateAnswer(msg) --> answer tiene la respuesta del chat, pero si lo descomento su respuesta es siempre que cargue ticket en jira
+            const {answer} = await generateAnswer(msg) //--> answer tiene la respuesta del chat, pero si lo descomento su respuesta es siempre que cargue ticket en jira
             //sospecho de que le esta llegando mal formateado el mensaje.
             console.log("Cuarto Paso: El servidor devuelve el mensaje hacia la pc del cliente.")
-            io.emit('chat message', ` ${msg}`);
+            io.emit('chat message', ` ${answer}`);
         });
     })
 }
